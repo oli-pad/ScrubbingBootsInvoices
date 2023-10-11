134c106609e2f8c33de5e7d6c1ca98f10f11a52a
oli-pad <oliver.oakes@people-discover.com>
2023-10-11 15:15:18 +0100

    update location

--- src/main.py
+++ src/main.py
@@ -9,9 +9,9 @@ import pandas as pd
 #    r"C:\Users\Oliver.Oakes.SALITIX\OneDrive - Salitix\Desktop\0000100401_000101QOD_Boots UK Supplier Funding Invoice 0090335610.pdf","CL002","MOR01").Full_Invoice(
 #    ).to_csv(r"C:\Users\Oliver.Oakes.SALITIX\OneDrive - Salitix\Desktop\Boots.csv")
 
-RenameInvoices(r"W:\Audit\Loreal\Invoice Images\EmailStagingBay\Boots")
+#RenameInvoices(r"W:\Audit\Loreal\Invoice Images\EmailStagingBay\Boots")
 
-#df = Get_Scrubbed_Detail('W:\Audit\Loreal\Invoice Images\EmailStagingBay\Boots','CL031','BOO01')
+df = Get_Scrubbed_Detail('W:\Audit\Loreal\Invoice Images\ImageStagingBay\Boots','CL031','BOO01')
 
 #For faster testing CSV to check the data in the dataframe
-#df.to_csv('W:\Audit\Loreal\Boots.csv',index=False)
+df.to_csv('W:\Audit\Loreal\Boots.csv',index=False)
